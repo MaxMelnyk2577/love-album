88da09578626623017fa2d7e79a8fc2ddafca805
MaxMelnyk2577 <152302138+MaxMelnyk2577@users.noreply.github.com>
2026-09-19 13:18:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,8 @@ MEMORIES = [
 def index():
     return render_template('index.html', memories=MEMORIES)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
